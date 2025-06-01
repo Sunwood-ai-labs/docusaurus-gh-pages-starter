3cbf502a64fe96477173b2c9fa661bace56501e5
Your Name <you@example.com>
2025-06-01 17:27:58 +0900

    ✨ faviconのパスのタイポを修正

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Docusaurus GitHub Pages Starter',
   tagline: 'Effortless documentation with automated deployment',
-  favicon: 'img/favicon-Pteranodlogoon.ico',
+  favicon: 'img/favicon-Pteranodon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
